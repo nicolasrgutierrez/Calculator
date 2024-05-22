679c05430b60c2621baa92bf87c608277ce6e61f
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2024-05-22 11:39:10 -0500

    Created code to display user inputted number, decimal, and add class when operator keys are pressed

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
 const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculatorKeys');
+const display = document.querySelector('.calculatorDisplay');
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         //Do Something
         const key = e.target;
         const action = key.dataset.action;
+        const keyContent = key.textContent;
+        const displayedNum = display.textContent;
+        
+        if (!action) {
+            // console.log('number key!')
+            if (displayedNum === '0') {
+                display.textContent = keyContent
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+        }
 
-    if (!action) {
-        console.log('number key!')
-    }
-    if (
-        action === 'add' ||
-        action === 'subtract' ||
-        action === 'multiply' ||
-        action === 'divide'
-    ) {
-        console.log('operator key!');
-    }
-    if(action === 'decimal') {
-        console.log('decimal key!');
-    }
-    if(action === 'clear') {
-        console.log('clear key!');
-    }
-    if(action === 'calculate') {
-        console.log('equal key!');
-    }
+        if (action === 'decimal') {
+            display.textContent = displayedNum + '.'
+        }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+        ) {
+            key.classList.add('is-depressed');
+            // Add custome atribute
+            calculator.dataset.previousKeyType = 'operator'
+        }
+
+        Array.from(key.parentNode.children).forEach(k =>
+            k.classList.remove('is-depressed'),
+            )
+        
+            
+        // Used to test if a number is being pressed
+        // if (
+        //     action === 'add' ||
+        //     action === 'subtract' ||
+        //     action === 'multiply' ||
+        //     action === 'divide'
+        // ) {
+        //     console.log('operator key!');
+        // }
+        // if (action === 'decimal') {
+        //     console.log('decimal key!');
+        // }
+        // if (action === 'clear') {
+        //     console.log('clear key!');
+        // }
+        // if (action === 'calculate') {
+        //     console.log('equal key!');
+        // }
     }
 })
