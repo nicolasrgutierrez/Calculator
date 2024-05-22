a096fc00c338621623511c1c69b3201cd9be5adf
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2024-05-22 12:15:16 -0500

    Completed basic math functions for calculator

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const calculator = document.querySelector('.calculator');
 const keys = calculator.querySelector('.calculatorKeys');
 const display = document.querySelector('.calculatorDisplay');
 
+
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
         //Do Something
@@ -9,10 +10,11 @@ keys.addEventListener('click', e => {
         const action = key.dataset.action;
         const keyContent = key.textContent;
         const displayedNum = display.textContent;
-        
+        const previousKeyType = calculator.dataset.previousKeyType;
+
         if (!action) {
             // console.log('number key!')
-            if (displayedNum === '0') {
+            if (displayedNum === '0' || previousKeyType === 'operator') {
                 display.textContent = keyContent
             } else {
                 display.textContent = displayedNum + keyContent
@@ -29,6 +31,8 @@ keys.addEventListener('click', e => {
             action === 'multiply' ||
             action === 'divide'
         ) {
+            calculator.dataset.firstValue = displayedNum;
+            calculator.dataset.operator = action;
             key.classList.add('is-depressed');
             // Add custome atribute
             calculator.dataset.previousKeyType = 'operator'
@@ -36,9 +40,34 @@ keys.addEventListener('click', e => {
 
         Array.from(key.parentNode.children).forEach(k =>
             k.classList.remove('is-depressed'),
-            )
-        
-            
+        )
+
+        if (action === 'calculate') {
+            const firstValue = calculator.dataset.firstValue;
+            const operator = calculator.dataset.operator;
+            const secondValue = displayedNum;
+
+            const calculate = (n1, operator, n2) => {
+                let result = '';
+
+                if (operator === 'add') {
+                    result = parseFloat(n1) + parseFloat(n2)
+                } else if (operator === 'subtract') {
+                    result = parseFloat(n1) - parseFloat(n2)
+                } else if (operator === 'multiply') {
+                    result = parseFloat(n1) * parseFloat(n2)
+                } else if (operator === 'divide') {
+                    result = parseFloat(n1) / parseFloat(n2)
+                }
+
+                return result;
+            }
+
+            display.textContent = calculate(firstValue, operator, secondValue);
+
+        }
+
+
         // Used to test if a number is being pressed
         // if (
         //     action === 'add' ||
