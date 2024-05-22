83f96cfa51b3f56198e59d9a55c1a9389f2ba4c5
Nicolas Gutierrez <nicolas.rene.gutierrez@gmail.com>
2024-05-22 13:38:03 -0500

    Fixed issues with repeated operator key presses and equal key. Also fixed issues with certain key sequences

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
-const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculatorKeys');
-const display = document.querySelector('.calculatorDisplay');
+const calculate = (n1, operator, n2) => {
+    let result = ''
+    if (operator === 'add') {
+        result = parseFloat(n1) + parseFloat(n2)
+    } else if (operator === 'subtract') {
+        result = parseFloat(n1) - parseFloat(n2)
+    } else if (operator === 'multiply') {
+        result = parseFloat(n1) * parseFloat(n2)
+    } else if (operator === 'divide') {
+        result = parseFloat(n1) / parseFloat(n2)
+    }
 
+    return result
+}
+
+const calculator = document.querySelector('.calculator')
+const display = calculator.querySelector('.calculatorDisplay')
+const keys = calculator.querySelector('.calculatorKeys')
 
 keys.addEventListener('click', e => {
     if (e.target.matches('button')) {
-        //Do Something
-        const key = e.target;
-        const action = key.dataset.action;
-        const keyContent = key.textContent;
-        const displayedNum = display.textContent;
-        const previousKeyType = calculator.dataset.previousKeyType;
+        const key = e.target
+        const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+        const previousKeyType = calculator.dataset.previousKeyType
+
+        Array.from(key.parentNode.children)
+            .forEach(k => k.classList.remove('is-depressed'))
 
         if (!action) {
-            // console.log('number key!')
-            if (displayedNum === '0' || previousKeyType === 'operator') {
+            if (
+                displayedNum === '0' || 
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate'
+            ) {
                 display.textContent = keyContent
             } else {
                 display.textContent = displayedNum + keyContent
             }
+            calculator.dataset.previousKeyType = 'number';
         }
 
         if (action === 'decimal') {
-            display.textContent = displayedNum + '.'
+            if (!displayedNum.includes('.')) {
+                display.textContent = displayedNum + '.'
+            } else if (
+                previousKeyType === 'operator' ||
+                previousKeyType === 'calculate'
+            ) {
+                display.textContent = '0.';
+            }
+
+            calculator.dataset.previousKeyType = 'decimal';
         }
 
         if (
@@ -31,60 +60,71 @@ keys.addEventListener('click', e => {
             action === 'multiply' ||
             action === 'divide'
         ) {
-            calculator.dataset.firstValue = displayedNum;
-            calculator.dataset.operator = action;
-            key.classList.add('is-depressed');
-            // Add custome atribute
-            calculator.dataset.previousKeyType = 'operator'
-        }
-
-        Array.from(key.parentNode.children).forEach(k =>
-            k.classList.remove('is-depressed'),
-        )
-
-        if (action === 'calculate') {
             const firstValue = calculator.dataset.firstValue;
             const operator = calculator.dataset.operator;
             const secondValue = displayedNum;
 
-            const calculate = (n1, operator, n2) => {
-                let result = '';
+            if (
+                firstValue && 
+                operator && 
+                previousKeyType !== 'operator' &&
+                previousKeyType !== 'calculate'
+            ) {
+                const calcValue = calculate(firstValue, operator, secondValue);
+                display.textContent = calcValue;
 
-                if (operator === 'add') {
-                    result = parseFloat(n1) + parseFloat(n2)
-                } else if (operator === 'subtract') {
-                    result = parseFloat(n1) - parseFloat(n2)
-                } else if (operator === 'multiply') {
-                    result = parseFloat(n1) * parseFloat(n2)
-                } else if (operator === 'divide') {
-                    result = parseFloat(n1) / parseFloat(n2)
-                }
-
-                return result;
+                //Update calculated value as firstValue
+                calculator.dataset.firstValue = calcValue;
+            } else {
+                // If there are no calculations, set displayedNum as the firstValue
+                calculator.dataset.firstValue = displayedNum;
             }
 
-            display.textContent = calculate(firstValue, operator, secondValue);
+            key.classList.add('is-depressed')
+            calculator.dataset.previousKeyType = 'operator';
+            calculator.dataset.operator = action;
+        }
+
+        if (action === 'clear') {
+            // console.log('clear key!')
+            calculator.dataset.previousKeyType = 'clear';
 
         }
 
+        if (action === 'calculate') {
+            let firstValue = calculator.dataset.firstValue
+            const operator = calculator.dataset.operator
+            let secondValue = displayedNum
 
-        // Used to test if a number is being pressed
-        // if (
-        //     action === 'add' ||
-        //     action === 'subtract' ||
-        //     action === 'multiply' ||
-        //     action === 'divide'
-        // ) {
-        //     console.log('operator key!');
-        // }
-        // if (action === 'decimal') {
-        //     console.log('decimal key!');
-        // }
-        // if (action === 'clear') {
-        //     console.log('clear key!');
-        // }
-        // if (action === 'calculate') {
-        //     console.log('equal key!');
-        // }
+            if (firstValue) {
+                if(previousKeyType === 'calculate') {
+                    firstValue = displayedNum
+                    secondValue = calculator.dataset.modValue
+                }
+                display.textContent = calculate(firstValue, operator, secondValue);
+            }
+            
+            //Set modValue attribute
+            calculator.dataset.modValue = secondValue;
+            calculator.dataset.previousKeyType = 'calculate';
+        }
     }
 })
+// Used to test if a number is being pressed
+// if (
+//     action === 'add' ||
+//     action === 'subtract' ||
+//     action === 'multiply' ||
+//     action === 'divide'
+// ) {
+//     console.log('operator key!');
+// }
+// if (action === 'decimal') {
+//     console.log('decimal key!');
+// }
+// if (action === 'clear') {
+//     console.log('clear key!');
+// }
+// if (action === 'calculate') {
+//     console.log('equal key!');
+// }
